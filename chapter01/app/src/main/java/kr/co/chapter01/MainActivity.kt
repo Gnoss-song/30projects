5a5ab1ad0f12441bd4ff992fc79f31dd65960cab
Gnoss-song <79887378+Gnoss-song@users.noreply.github.com>
2021-08-27 11:40:28 +0900

    Refactoring dataBinding

--- chapter01/app/src/main/java/kr/co/chapter01/MainActivity.kt
+++ chapter01/app/src/main/java/kr/co/chapter01/MainActivity.kt
@@ -9,25 +9,24 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import kr.co.chapter01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val heightEditText : EditText = findViewById(R.id.heightEditText)
-        val weightEditText = findViewById<EditText>(R.id.weightEditText)
 
-        val resultButton = findViewById<Button>(R.id.resultButton)
-
-        resultButton.setOnClickListener {
-            if (heightEditText.text.isEmpty() || weightEditText.text.isEmpty()){
+        binding.resultButton.setOnClickListener {
+            if (binding.heightEditText.text.isEmpty() || binding.weightEditText.text.isEmpty()){
                 Toast.makeText(this,"빈 값이 있습니다.",Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             // 예외처리가 끝난 후의 상황. 아래로는 빈 값이 올 수 없음.
-            val height = heightEditText.text.toString().toInt()
-            val weight = weightEditText.text.toString().toInt()
+            val height = binding.heightEditText.text.toString().toInt()
+            val weight = binding.weightEditText.text.toString().toInt()
 
             val intent = Intent(this,ResultActivity::class.java)
             intent.putExtra("height",height)
